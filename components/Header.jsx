6e72477380cc56c728e59d6c89cee3ce8e99a8a3
Xurr1C4n3-byte <xurr1C4n3@gmail.com>
2025-11-05 18:17:15 +0630

    Add Header component with globe icon and title

---
+++ components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+export default function Header() {
+    return (
+        <header>
+            <img src="../images/globe.png" alt="globe icon" />
+            <h1>my travel journal.</h1>
+        </header>
+    )
+}
